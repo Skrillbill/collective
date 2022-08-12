20369b423745bc65d06d9ed9b84ea6cf4ef60eb1
skrillex <skrill@t3industries.net>
2022-08-12 13:25:49 -0400

    encrypted password generator

---
+++ powershell_projects/encryptPassword.ps1
@@ -0,0 +1,11 @@
+﻿#
+#$credential = Get-Credential
+#$credential.Password | ConvertFrom-SecureString | Set-Content -Path 'C:\Powershell Script\encryptedPword_string.txt'
+#
+
+
+$keyHash = Get-Content -Path 'C:\Powershell Script\encryptedPword_string.txt' | ConvertTo-SecureString
+$credential = New-Object System.Management.Automation.PsCredential('null', $encrypted)
+
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($credential.Password)
+$pWord = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
